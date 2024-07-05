add585aaca3dd2a1faf7deee16797eb8b0690639
Vignesh <viwanathan.vignesh@heb.com>
2024-07-05 22:00:18 +0530

    Footer changed

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -9,12 +9,12 @@ export const Footer = () => {
       <div className="items-center justify-between px-10 py-4 mx-auto sm:px-6 row max-w-7xl">
         <div className="items-center gap-4 row">
           <Image
-            src="/images/logo.png"
-            alt="Vivid Logo"
+            src="/icons/robzlogo.png"
+            alt="Robs apps"
             height="40"
             width="40"
           />
-          <div className="text-sm text-light">&copy; Vivid Labs, Inc. 2022</div>
+          <div className="text-sm text-light">&copy; robzapps, Inc. 2022</div>
         </div>
         <LinkButton
           href="https://join.slack.com/t/vivid-jqa6824/shared_invite/zt-1f7tbxl86-wruO1aM7FYmEdNaOOnYCMQ"
