900db14d490e80e5487f68a6053840d6bcd4322b
Vignesh <viwanathan.vignesh@heb.com>
2024-07-06 12:24:48 +0530

    removed conifg

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/template",
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/template",
-        basePath: false,
-        permanent: false,
-      },
-    ];
   },
 };
 
