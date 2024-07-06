46cced4f764a0f8bad8c2bb18483bbc45bbc3a31
Vignesh <viwanathan.vignesh@heb.com>
2024-07-06 12:11:28 +0530

    basePath 2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/template",
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-  basePath: "/template",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/template",
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
